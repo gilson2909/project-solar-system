b68e81287fbff4de97c3f750d54081feeb5760e9
gilson2909 <andradegilson2909@gmail.com>
2023-05-10 18:04:24 -0300

    questão:08

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Title from './Title';
 
 class Missions extends React.Component {
   render() {
     return (
-      <div data-testid="missions" />
+      <div data-testid="missions">
+        <div>
+          <Title headline="Missões" />
+        </div>
+      </div>
     );
   }
 }
