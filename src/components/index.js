b8f5a2f9e281726b4fddca6da9fc620b74bac5ce
gilson2909 <andradegilson2909@gmail.com>
2023-05-10 10:29:34 -0300

    retentando

--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 class Header extends React.Component {
   render() {
-    return (<header><h1>Sistema Solar</h1></header>);
+    return (
+      <header>
+        <h1>Sistema Solar</h1>
+      </header>
+    );
   }
 }
 
